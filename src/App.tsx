473fc1372d5be6d50477ba51cd41b3a4f67906d3
Nikolay Tzolov <ntzolov@gmail.com>
2024-05-27 21:40:08 +0300

    add user logic with ip

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { useEffect, useState } from 'react';
 import CreateFact from './components/CreateFact';
 
 function App() {
+  const [user, setUser] = useState('');
+
+  useEffect(() => {
+    fetch('https://api.ipify.org/?format=json')
+      .then((res) => res.json())
+      .then((data) => setUser(data.ip));
+  }, []);
+
   return (
     <div className='flex flex-col items-center bg-gray-800'>
-      <CreateFact />
+      <CreateFact user={user} />
     </div>
   );
 }
